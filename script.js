5720b496afa3be92ba46f09f2fd28ddb69cff689
Vladimir Astrelin <43417867+VladimirAstrelin@users.noreply.github.com>
2026-05-07 12:24:13 +0300

    Main page refactoring

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function loadProject(path) {
-    document.getElementById('projectFrame').src = path;
+    const welcomeScreen = document.getElementById('welcomeScreen');
+    const projectFrame = document.getElementById('projectFrame');
+    
+    welcomeScreen.style.display = 'none';
+    projectFrame.style.display = 'block';
+    projectFrame.src = path;
 }
